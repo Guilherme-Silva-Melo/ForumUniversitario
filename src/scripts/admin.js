fdf6e45a6d5292e0b8a9ddb915c1d75a6843c545
Guilherme Schmidt <guischmidt201@gmail.com>
2024-06-21 01:43:02 -0300

    add: modal em uma ação

--- src/scripts/admin.js
+++ src/scripts/admin.js
@@ -6,29 +6,20 @@ document.addEventListener('DOMContentLoaded', function() {
   const searchField = document.getElementById('searchField');
   const searchQuery = document.getElementById('searchQuery');
   const searchButton = document.getElementById('searchButton');
-
   const clearButton = document.getElementById('clearField-button');
   const formCadastro = document.getElementsByClassName('signup')[0];
 
-  
   function renderUserList(users = JSON.parse(localStorage.getItem('users')) ) {
-    
     userList.innerHTML = '';
-  
     users.forEach(user => {
       const li = document.createElement('li');
       li.classList.add('user-item'); // Adiciona a classe para centralizar o conteúdo
-      
-      // Criar botão de excluir
+
       const deleteButton = document.createElement('button');
       deleteButton.textContent = 'Excluir';
       deleteButton.classList.add('delete-button');
-
-      // Adicionar evento de clique para excluir o item
       deleteButton.addEventListener('click', () => {
-
         userList.removeChild(li);
-        
         const updatedUsers = users.filter(u => u.email !== user.email);
         localStorage.setItem('users', JSON.stringify(updatedUsers));
         renderUserList(updatedUsers);
@@ -37,14 +28,14 @@ document.addEventListener('DOMContentLoaded', function() {
       li.appendChild(deleteButton);
       li.appendChild(document.createTextNode(` Data: ${user.date}, Nome: ${user.username}, E-mail: ${user.email}`));
       userList.appendChild(li);
+      
     });
   }
 
   function limparLista() {
     userList.innerHTML = ''; // Limpa todos os itens da lista no HTML
     localStorage.removeItem('users'); // Remove o item 'users' do local storage
-    message.textContent = 'Lista de usuários foi limpa.';
-    message.style.color = 'orange';
+    exibirModal('Lista de usuários foi limpa.', 'orange');
   }
 
   function searchUserList(field, query) {
@@ -62,15 +53,13 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   }
 
-  // Adiciona um ouvinte de evento para o botão "Limpar Lista"
-  clearButton.addEventListener('click', function() {
+  clearButton.addEventListener('click', function(event) {
     event.preventDefault();  // Evita que o checkbox seja marcado
     limparCampos();
   });
 
-  // Adiciona um ouvinte de evento para o botão "Limpar Lista"
   clearListButton.addEventListener('click', function() {
-      limparLista();
+    limparLista();
   });
 
   searchButton.addEventListener('click', function() {
@@ -79,47 +68,43 @@ document.addEventListener('DOMContentLoaded', function() {
     searchUserList(field, query);
   });
 
-  // Adiciona um ouvinte de evento para o formulário de usuário
   userForm.addEventListener('submit', function(event) {
     event.preventDefault();
-
     const username = document.getElementById('username').value;
     const email = document.getElementById('email').value;
-
     if (username && email) {
-      // Cria um objeto de usuário com nome, email e data atual
       const user = {
         username: username,
         email: email,
-        date: new Date().toLocaleDateString() // Adiciona a data de envio
+        date: new Date().toLocaleDateString()
       };
-
-      // Recupera usuários existentes do localStorage ou cria um novo array vazio
       let users = JSON.parse(localStorage.getItem('users')) || [];
-
-      // Adiciona o novo usuário ao array
       users.push(user);
-
-      // Salva o array atualizado de usuários no localStorage
       localStorage.setItem('users', JSON.stringify(users));
-
-      // Renderiza a lista atualizada de usuários
       renderUserList(users);
-
-      // Exibe mensagem de sucesso
-      message.textContent = 'Usuário cadastrado com sucesso!';
-      message.style.color = 'green';
-
-      
+      exibirModal('Usuário cadastrado com sucesso!', 'green');
       userForm.reset();
     } else {
-      
-      message.textContent = 'Por favor, preencha todos os campos.';
-      message.style.color = 'red';
+      exibirModal('Por favor, preencha todos os campos.', 'red');
     }
   });
 
-  
+  function exibirModal(mensagem, cor) {
+    var modal = document.getElementById("modal");
+    var modalMessage = document.getElementById("modal-message");
+    var closeBtn = document.getElementsByClassName("close")[0];
+    modalMessage.textContent = mensagem;
+    modalMessage.style.color = cor;
+    modal.style.display = "block";
+    closeBtn.onclick = function() {
+      modal.style.display = "none";
+    }
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
+  }
+
   renderUserList();
 });
-  
